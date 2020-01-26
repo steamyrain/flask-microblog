73f0dadc44085fffbdc017b5a0306e3a57877a46
xiaoren <reinz.rawk@gmail.com>
2020-01-26 15:17:40 +0700

    add navbar

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username':'Ren'}
+    posts = [
+                {
+                    'author':{'username':'susan'},
+                    'body':'The avengers movie was so cool!'
+                },
+                {
+                    'author':{'username':'john'},
+                    'body':'Beautiful day in Portland!'
+                }
+            ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
+
